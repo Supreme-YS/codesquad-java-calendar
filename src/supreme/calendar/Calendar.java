b26df431d9cc9b00a826a2b8fc8b9f471442e426
Supreme-YS <dudtjr1225@gmail.com>
2021-09-02 12:51:38 +0900

    Calendar Done :)

--- src/supreme/calendar/Calendar.java
+++ src/supreme/calendar/Calendar.java
@@ -19,19 +19,36 @@ public class Calendar {
             return MAX_DAYS[month - 1];
     }
 
-    public void printCalendar(int year, int month) {
+    public void printCalendar(int year, int month, int weekday) {
         System.out.printf("    << %4d년 %3d >>    \n", year, month);
         System.out.println(" SU MO TU WE TH FR SA");
         System.out.println("-----------------");
 
-        int maxDay = getMaxDaysOfMonth(year, month);
+        // print black space
+        for (int i = 0; i < weekday; i++) {
+            System.out.print("   ");
+        }
 
-        for (int i = 1; i <= maxDay; i++) {
+        int maxDay = getMaxDaysOfMonth(year, month);
+        int count = 7 - weekday;
+        int delim = count;
+
+        // print first line
+        for (int i = 1; i <= count; i++) {
             System.out.printf("%3d", i);
-            if (i % 7 == 0) {
+        }
+        System.out.println();
+
+        count++;
+
+        // print from second line to last
+        for (int i = count; i <= maxDay; i++) {
+            System.out.printf("%3d", i);
+            if (i % 7 == delim) {
                 System.out.println();
             }
         }
+        System.out.println();
         System.out.println();
     }
 }
--- src/supreme/calendar/Prompt.java
+++ src/supreme/calendar/Prompt.java
@@ -3,8 +3,22 @@ package supreme.calendar;
 import java.util.Scanner;
 
 public class Prompt {
-
-    private final static String PROMPT = "> ";
+    /**
+     *
+     * @param week 요일명
+     * @return 0 ~ 6 (0 = Sunday, 6 = Saturday)
+     */
+    public int parseDay(String week) {
+        if (week.equals("SU") ) return 0;
+        else if (week.equals("MO")) return 1;
+        else if (week.equals("TU")) return 2;
+        else if (week.equals("WE")) return 3;
+        else if (week.equals("TH")) return 4;
+        else if (week.equals("FR")) return 5;
+        else if (week.equals("SA")) return 6;
+        else
+            return 0;
+    }
 
     public void runPrompt() {
         Scanner scanner = new Scanner(System.in);
@@ -12,14 +26,18 @@ public class Prompt {
 
         int month;
         int year;
+        int weekday;
 
         while (true) {
             System.out.println("연도를 입력하세요");
-            System.out.print(PROMPT);
+            System.out.print("YEAR  > ");
             year = scanner.nextInt();
             System.out.println("달을 입력하세요");
-            System.out.print(PROMPT);
+            System.out.print("MONTH > ");
             month = scanner.nextInt();
+            System.out.println("첫째날의 요일을 입력하세요.(SU, MO, TU, WE, TH, FR, SA).");
+            String str_weekday = scanner.next();
+            weekday = parseDay(str_weekday);
 
             if (month <= 0) {
                 break;
@@ -27,7 +45,7 @@ public class Prompt {
                 System.out.println("1부터 12사이의 숫자를 입력하세요");
                 continue;
             }
-            cal.printCalendar(year, month);
+            cal.printCalendar(year, month, weekday);
         }
 
         System.out.println("프로그램 종료 :D");
@@ -35,7 +53,7 @@ public class Prompt {
     }
 
     public static void main(String[] args) {
-        // 셀 실행
+        // execute cell
         Prompt p = new Prompt();
         p.runPrompt();
     }
