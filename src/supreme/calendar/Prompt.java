beec7a58992b13c3245fbab66867405de3088393
Supreme-YS <dudtjr1225@gmail.com>
2021-09-02 12:18:39 +0900

    Add LeapDays functions

--- src/supreme/calendar/Prompt.java
+++ src/supreme/calendar/Prompt.java
@@ -4,15 +4,19 @@ import java.util.Scanner;
 
 public class Prompt {
 
-    private final static String PROMPT = "CAL > ";
+    private final static String PROMPT = "> ";
 
     public void runPrompt() {
         Scanner scanner = new Scanner(System.in);
         Calendar cal = new Calendar();
 
-        int month ;
+        int month;
+        int year;
 
         while (true) {
+            System.out.println("연도를 입력하세요");
+            System.out.print(PROMPT);
+            year = scanner.nextInt();
             System.out.println("달을 입력하세요");
             System.out.print(PROMPT);
             month = scanner.nextInt();
@@ -23,7 +27,7 @@ public class Prompt {
                 System.out.println("1부터 12사이의 숫자를 입력하세요");
                 continue;
             }
-            cal.printCalendar(2021, month);
+            cal.printCalendar(year, month);
         }
 
         System.out.println("프로그램 종료 :D");
